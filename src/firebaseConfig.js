3135dba06ca0d0ba94a7cefee75e468c2d5df4ab
MichelleFMB <102604395+MichelleFMB@users.noreply.github.com>
2024-07-17 22:26:26 -0500

    feat - firebase configuration

---
+++ src/firebaseConfig.js
@@ -0,0 +1,24 @@
+import { initializeApp } from "firebase/app"; 
+import { getAuth, GoogleAuthProvider, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCMyam01aFinTD_UO1pcDnYnTxx9AxTvFI",
+  authDomain: "webapp-6d2a0.firebaseapp.com",
+  projectId: "webapp-6d2a0",
+  storageBucket: "webapp-6d2a0.appspot.com",
+  messagingSenderId: "314942183186",
+  appId: "1:314942183186:web:961f8075fec4c43b5d45cb",
+  measurementId: "G-2NQ3DCQ7CJ"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
+
+export const registerWithEmail = (email, password) => {
+  return createUserWithEmailAndPassword(auth, email, password);
+};
+
+export const loginWithEmail = (email, password) => {
+  return signInWithEmailAndPassword(auth, email, password);
+};
