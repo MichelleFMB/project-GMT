6ba2cc02b00ea6b1763830a62e6139987296f878
MichelleFMB <102604395+MichelleFMB@users.noreply.github.com>
2024-07-19 20:59:40 -0500

    fix - firebaseConfig

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCMyam01aFinTD_UO1pcDnYnTxx9AxTvFI",
